61effe23c0d75dba89547085483e5203233d6b11
Daniuwu504 <74873220+Daniuwu504@users.noreply.github.com>
2021-08-20 03:51:07 -0600

    Add project files.

---
+++ App003SQLite/App003SQLite/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SQLite;
+using App003SQLite.model;
+using App003SQLite.controller;
+using App003SQLite.ModelView;
+
+namespace App003SQLite
+{
+    public partial class MainPage : ContentPage
+    {  
+ 
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new PersonsViewModel();
+        }
+
+   
+     
+        private async void lista_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new home());
+        }
+    }
+}
